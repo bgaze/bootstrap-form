d8a1f43a81d7df14062bf1149ea234efe8779383
Bgaze <benjamin@bgaze.fr>
2019-11-11 12:19:23 +0400

    Add comments

--- src/Support/Traits/HasAddons.php
+++ src/Support/Traits/HasAddons.php
@@ -8,30 +8,36 @@ namespace Bgaze\BootstrapForm\Support\Traits;
  * @property mixed $prepend
  * @property mixed $append
  */
-trait HasAddons {
+trait HasAddons
+{
 
     /**
      * Build a Bootstrap input group if append or prepend options are provided.
      * 
      * @return string
      */
-    public function inputGroup() {
+    public function inputGroup()
+    {
+        // Check if addons.
         if (!$this->append && !$this->prepend) {
             return $this->input();
         }
 
+        // Prepare prepend group.
         $prepend = '';
         if ($this->prepend) {
             $content = is_array($this->prepend) ? implode('', $this->prepend) : $this->prepend;
             $prepend = $this->html->tag('div', $content, ['class' => 'input-group-prepend']);
         }
 
+        // Prepare append group.
         $append = '';
         if ($this->append) {
             $content = is_array($this->append) ? implode('', $this->append) : $this->append;
             $append = $this->html->tag('div', $content, ['class' => 'input-group-append']);
         }
 
+        // Prepare group class.
         $class = 'input-group';
         if ($this->size === 'sm') {
             $class .= ' input-group-sm';
@@ -39,7 +45,7 @@ trait HasAddons {
             $class .= ' input-group-lg';
         }
 
+        // Wrap elements.
         return $this->html->tag('div', $prepend . $this->input() . $append, ['class' => $class])->toHtml();
     }
-
 }
